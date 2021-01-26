7779576b00c8750123793e31647c0b57579a9cd1
FrancescoVassalli <vassalli@LAPTOP-IP1831BD.localdomain>
2021-01-26 09:49:42 -0700

    debugging price

--- games/AT.py
+++ games/AT.py
@@ -42,7 +42,7 @@ class MuZeroConfig:
         #for each stock we can buy or sell we can also do nothing
         self.action_space = list(range(2*g_nStocks+1))  # Fixed list of all possible actions. You should only edit the length
         self.players = list(range(1))  # List of players. You should only edit the length
-        self.stacked_observations = 0  # Number of previous observations and previous actions to add to the current observation
+        self.stacked_observations = 10  # Number of previous observations and previous actions to add to the current observation
 
         # Evaluate
         self.muzero_player = 0  # Turn Muzero begins to play (0: MuZero plays first, 1: MuZero plays second)
@@ -52,8 +52,8 @@ class MuZeroConfig:
         ### Self-Play
         self.num_workers = 1  # Number of simultaneous threads/workers self-playing to feed the replay buffer
         self.selfplay_on_gpu = False
-        self.max_moves = 6  # Maximum number of moves if game is not finished before
-        self.num_simulations = 10  # Number of future moves self-simulated
+        self.max_moves = 126  # Maximum number of moves if game is not finished before
+        self.num_simulations = 3  # Number of future moves self-simulated
         self.discount = 0.997  # Chronological discount of the reward
         self.temperature_threshold = None  # Number of moves before dropping the temperature given by visit_softmax_temperature_fn to 0 (ie selecting the best action). If None, visit_softmax_temperature_fn is used every time
 
@@ -69,21 +69,21 @@ class MuZeroConfig:
 
         ### Network
         self.network = "fullyconnected"  # "resnet" / "fullyconnected"
-        self.support_size = 10  # Value and reward are scaled (with almost sqrt) and encoded on a vector with a range of -support_size to support_size. Choose it so that support_size <= sqrt(max(abs(discounted reward)))
+        self.support_size = 20  # Value and reward are scaled (with almost sqrt) and encoded on a vector with a range of -support_size to support_size. Choose it so that support_size <= sqrt(max(abs(discounted reward)))
         
         # Residual Network
         self.downsample = False  # Downsample observations before representation network, False / "CNN" (lighter) / "resnet" (See paper appendix Network Architecture)
-        self.blocks = 1  # Number of blocks in the ResNet
-        self.channels = 2  # Number of channels in the ResNet
-        self.reduced_channels_reward = 2  # Number of channels in reward head
-        self.reduced_channels_value = 2  # Number of channels in value head
-        self.reduced_channels_policy = 2  # Number of channels in policy head
-        self.resnet_fc_reward_layers = []  # Define the hidden layers in the reward head of the dynamic network
-        self.resnet_fc_value_layers = []  # Define the hidden layers in the value head of the prediction network
-        self.resnet_fc_policy_layers = []  # Define the hidden layers in the policy head of the prediction network
+        self.blocks = 16  # Number of blocks in the ResNet
+        self.channels = 256  # Number of channels in the ResNet
+        self.reduced_channels_reward = 256  # Number of channels in reward head
+        self.reduced_channels_value = 256  # Number of channels in value head
+        self.reduced_channels_policy = 256  # Number of channels in policy head
+        self.resnet_fc_reward_layers = [256,256]  # Define the hidden layers in the reward head of the dynamic network
+        self.resnet_fc_value_layers = [256,256]  # Define the hidden layers in the value head of the prediction network
+        self.resnet_fc_policy_layers = [256,256]  # Define the hidden layers in the policy head of the prediction network
 
         # Fully Connected Network
-        self.encoding_size = 5
+        self.encoding_size = 8
         self.fc_representation_layers = [16]  # Define the hidden layers in the representation network
         self.fc_dynamics_layers = [16]  # Define the hidden layers in the dynamics network
         self.fc_reward_layers = [16]  # Define the hidden layers in the reward network
@@ -98,7 +98,7 @@ class MuZeroConfig:
         self.training_steps = 10000  # Total number of training steps (ie weights update according to a batch)
         self.batch_size = 32  # Number of parts of games to train on at each training step
         self.checkpoint_interval = 10  # Number of training steps before using the model for self-playing
-        self.value_loss_weight = 1  # Scale the value loss to avoid overfitting of the value function, paper recommends 0.25 (See paper appendix Reanalyze)
+        self.value_loss_weight = 0.25  # Scale the value loss to avoid overfitting of the value function, paper recommends 0.25 (See paper appendix Reanalyze)
         self.train_on_gpu = True if torch.cuda.is_available() else False  # Train on GPU if available
         if self.train_on_gpu:
             print("On GPU")
@@ -117,9 +117,9 @@ class MuZeroConfig:
 
 
         ### Replay Buffer
-        self.replay_buffer_size = 500  # Number of self-play games to keep in the replay buffer
+        self.replay_buffer_size = 100  # Number of self-play games to keep in the replay buffer
         self.num_unroll_steps = 7  # Number of game moves to keep for every batch element
-        self.td_steps = 7  # Number of steps in the future to take into account for calculating the target value
+        self.td_steps = 31  # Number of steps in the future to take into account for calculating the target value
         self.PER = True  # Prioritized Replay (See paper appendix Training), select in priority the elements in the replay buffer which are unexpected for the network
         self.PER_alpha = 0.5  # How much prioritization is used, 0 corresponding to the uniform case, paper suggests 1
 
@@ -130,7 +130,7 @@ class MuZeroConfig:
 
 
         ### Adjust the self play / training ratio to avoid over/underfitting
-        self.self_play_delay = 0.2  # Number of seconds to wait after each played game
+        self.self_play_delay = 0.1  # Number of seconds to wait after each played game
         self.training_delay = 0  # Number of seconds to wait after each training step
         self.ratio = None  # Desired training steps per self played step ratio. Equivalent to a synchronous version, training can take much longer. Set it to None to disable it
 
@@ -232,17 +232,17 @@ class ATEnv:
         self.closes = [self.data.getPrices(True)]
         #get the features as a list of one DF per stock make sure the order is the same
         self.features = [self.data.getFeatures(True)]
-        self.max = self.data.getSize(True)
+        self.max = self.data.getSize(True)-1
         self.ownership = [0]*g_nStocks
         self.time =random.randrange(1, self.max)
-        print("Starting Env with max time = "+str(self.max))
+        print("Starting Env with max time = "+str(self.max)+ " time = "+str(self.time))
         print(self.features[0].head())
         self.cash = 1.0
         self.last_action = -1
         
     def legal_actions(self):
         # Initialize to all moves and then prune.
-        if self.getChangeValue()==0
+        if self.getChangeValue()==0:
             return [0]
         return list(range(2*g_nStocks+1))
 
@@ -267,14 +267,20 @@ class ATEnv:
                 
     def getReward(self):
         total = 0
+        #print("Reward = "+str(total)+"\nOwnership: "+str(self.ownership)+"\nLast action: "+str(self.last_action)+"\nCloses: "+str([self.closes[0][self.time],self.closes[0][self.time-1]]))
         for i in range(len(self.ownership)):
-            total += self.ownership[i]*(self.closes[i][self.time]-self.closes[i][self.time-1])/self.closes[i][self.time-1]
-        print("Reward = "+str(total)+"\nOwnership: "+str(self.ownership)+"\nLast action: "+str(self.last_action))
-        return 10*total
+            base = self.closes[i][self.time-1]
+            if base==0:
+                print("Base 0 at "+str(self.time-1))
+                total=0
+            else:
+                total += self.ownership[i]*(self.closes[i][self.time]-self.closes[i][self.time-1])/base
+        return 100*total
             
     def reset(self):
         self.ownership = [0 for i in range(g_nStocks)]
-        self.time = 1 
+        self.time = random.randrange(1, self.max) 
+        self.last_action = -1
         return self.get_observation()
 
     def render(self):
@@ -286,7 +292,7 @@ class ATEnv:
         observation = numpy.zeros((g_nStocks,g_nFeatures))
         #print("Getting observation at time = "+str(self.time))
         for i in range(len(self.ownership)):
-            #print("from stock "+str(i))
+            #print("time: "+str(self.time)+"\nmax: "+str(self.max))
             features  = self.features[i].iloc[[self.time]].values.flatten().tolist()
             #print("features: "+str(features))
             #print("Ownership: "+str(self.ownership[i]))
